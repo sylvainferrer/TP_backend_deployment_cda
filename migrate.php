cd47ce28306c79635e834cae6e67286ae524de6d
Sylvain <40465096+sylvainferrer@users.noreply.github.com>
2025-06-04 10:17:27 +0200

    Update migrate.php

--- migrate.php
+++ migrate.php
@@ -11,7 +11,7 @@ try {
     echo "Base de données 'storehoop_db' créée ou déjà existante.\n";
 
     // Se connecter à la base nouvellement créée
-    $pdo->exec("USE podcast_db");
+    $pdo->exec("USE storehoop_db");
 
     // Créer la table
     $sql = "CREATE TABLE IF NOT EXISTS podcasts (
